d36bf31a274ebcd092ff2ee6644970c4a9745fcf
Pixel Gui <guillaumethomart@gmail.com>
2024-02-01 22:28:58 +0100

    Script & Readme finalisé

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 Mise en place exercices simples
 
-- S'exercer en Js 
-- Utiliser le terminal et le versioning Github
+Exercice sur les conditions **if** / **else** / **switch**
+
+Écris un programme qui demande à l'utilisateur son âge et vérifie s'il est éligible à un tarif réduit au cinéma en fonction de son âge.
+
+- Les personnes de moins de 18 ans bénéficient d'un tarif réduit.
+- Les personnes âgées de 65 ans et plus bénéficient également d'un tarif réduit.
+- Toutes les autres personnes paient le tarif normal.
--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-let helloConsole = "Hello world !";
-console.log(helloConsole);
+
+// On demande à l'utilisateur quel est son âge 
+let age = prompt("Quel est votre âge ?")
+
+// Tarif appliqué selon son âge
+if (age < 18){
+    console.log("Vous bénéficiez d'un tarif réduit");
+}else if (age >= 65){
+    console.log("Vous bénéficiez d'un tarif senior");
+}else{
+    console.log("Vous payez plein pot");
+}
+
+// Version avec une condition switch
+
+switch (true) {
+    case (age < 18):
+    case (age >= 65):
+        console.log("Bravo ! Vous bénéficiez d'un tarif réduit");
+        break;
+        default :
+        console.log("Vous payez plein pot ! Dommage");
+}
+
